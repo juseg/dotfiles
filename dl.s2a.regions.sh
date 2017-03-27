5244cdd0d707ce7859cf4d4c9a1ae1c8c6bf66f6
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-03-27 14:46:53 +0200

    Move dl.s2a.sh to separate repo sentinelflow.
    Modify dl.s2a.regions.sh and dl.s2a.tiles.sh accordingly.

--- dl.s2a.regions.sh
+++ dl.s2a.regions.sh
@@ -1,122 +1,130 @@
 #!/bin/bash
 
-# Run parent script dl.s2a.sh on multiple regions
-# ===============================================
+# Run sentinelflow.sh on multiple regions
+# =======================================
 
-args="$*"
+# move to base directory
+cd $SCRATCH2/geodata/satellite/sentinel-2a
+
+# path to executable
+sf="$HOME/code/sentinelflow/sentinelflow.sh\
+ --user julien.seguinot --pass Cordillera $*"
+
 
 # Greenland
 # ---------
 
 # Qaanaaq 6000x6000 (intersect on Bowdoin Glacier)
-dl.s2a.sh --name "greenland/qaanaaq" --offline \
-          --intersect "77.7,-68.5" --tiles "19XDG,19XEG" \
-          --extent "465000,8595000,525000,8655000" --resolution "10" $args
+$sf --name greenland/qaanaaq --offline \
+    --intersect 77.7,-68.5 --tiles 19XDG,19XEG \
+    --extent 465000,8595000,525000,8655000 --resolution 10
 
 # Qeqertaq / Tracy 4000x6000
-dl.s2a.sh --name "greenland/qeqertat" --offline \
-          --intersect "77.5,-66.7" --tiles "19XEG" \
-          --extent "535000,8595000,595000,8635000" --resolution "10" $args
+$sf --name greenland/qeqertat --offline \
+    --intersect 77.5,-66.7 --tiles 19XEG \
+    --extent 535000,8595000,595000,8635000 --resolution 10
 
 # Hans Tausen Ice Cap 8000x8000 + 5000x8000
-dl.s2a.sh --name "greenland/hanstausen-utm23" \
-          --intersect "82.6,-38.2" --tiles "23XNN,23XNM" --cloudcover "30" \
-          --extent "529800,9130000,609800,9210000" --resolution "10" $args
-dl.s2a.sh --name "greenland/hanstausen-utm24" \
-          --intersect "82.6,-38.2" --tiles "24XWT,24XWS" --cloudcover "30" \
-          --extent "500000,9120000,550000,9200000" --resolution "10" $args
+$sf --name greenland/hanstausen-utm23 \
+    --intersect 82.6,-38.2 --tiles 23XNN,23XNM --cloudcover 30 \
+    --extent 529800,9130000,609800,9210000 --resolution 10
+$sf --name greenland/hanstausen-utm24 \
+    --intersect 82.6,-38.2 --tiles 24XWT,24XWS --cloudcover 30 \
+    --extent 500000,9120000,550000,9200000 --resolution 10
 
 # Europe
 # ------
 
 # Ecrins 3000x3000
-dl.s2a.sh --name "europe/ecrins" \
-          --intersect "44.9,6.3" --tiles "31TGK" \
-          --extent "745000,4965000,775000,4995000" --resolution "10" $args
+$sf --name europe/ecrins \
+    --intersect 44.9,6.3 --tiles 31TGK \
+    --extent 745000,4965000,775000,4995000 --resolution 10
 
 # Mt Blanc 3000x3000
-dl.s2a.sh --name "europe/mtblanc" --offline \
-          --intersect "45.9,7.0" --tiles "32TLR" \
-          --extent "325000,5070000,355000,5100000" --resolution "10" $args
+$sf --name europe/mtblanc --offline \
+    --intersect 45.9,7.0 --tiles 32TLR \
+    --extent 325000,5070000,355000,5100000 --resolution 10
 
 # Plaine Morte 1000x1000
-dl.s2a.sh --name "europe/plainemorte" --offline \
-          --intersect "46.4,7.5" --tiles "32TLS" \
-          --extent "380000,5135000,390000,5145000" --resolution "10" $args
+$sf --name europe/plainemorte --offline \
+    --intersect 46.4,7.5 --tiles 32TLS \
+    --extent 380000,5135000,390000,5145000 --resolution 10
 
 # Bern / Aletsch 6000x4000
-dl.s2a.sh --name "europe/bern" --offline \
-          --intersect "46.5,8.1" --tiles "32TMS" \
-          --extent "400000,5135000,460000,5175000" --resolution "10" $args
+$sf --name europe/bern --offline \
+    --intersect 46.5,8.1 --tiles 32TMS \
+    --extent 400000,5135000,460000,5175000 --resolution 10
 
 # Uri 3000x5000
-dl.s2a.sh --name "europe/uri" --offline \
-          --intersect "46.8,8.4" --tiles "32TMT,32TMS" \
-          --extent "445000,5155000,475000,5205000" --resolution "10" $args
+$sf --name europe/uri --offline \
+    --intersect 46.8,8.4 --tiles 32TMT,32TMS \
+    --extent 445000,5155000,475000,5205000 --resolution 10
 
 # Appenzell 4000x3000
-dl.s2a.sh --name "europe/appenzell" --offline \
-          --intersect "47.2,9.3" --tiles "32TNT" \
-          --extent "500000,5210000,540000,5240000" --resolution "10" $args
+$sf --name europe/appenzell --offline \
+    --intersect 47.2,9.3 --tiles 32TNT \
+    --extent 500000,5210000,540000,5240000 --resolution 10
 
 # Bernina 4000x3000
-dl.s2a.sh --name "europe/bernina" --offline \
-          --intersect "46.4,9.9" --tiles "32TNS" \
-          --extent "540000,5120000,580000,5150000" --resolution "10" $args
+$sf --name europe/bernina --offline \
+    --intersect 46.4,9.9 --tiles 32TNS \
+    --extent 540000,5120000,580000,5150000 --resolution 10
 
 # Pennine / Gorner 6000x4000
-dl.s2a.sh --name "europe/pennine" --offline \
-          --intersect "46.0,7.8" --tiles "32TLS,32TMS,32TLR,32TMR" \
-          --extent "360000,5080000,420000,5125000" --resolution "10" $args
+$sf --name europe/pennine --offline \
+    --intersect 46.0,7.8 --tiles 32TLS,32TMS,32TLR,32TMR \
+    --extent 360000,5080000,420000,5125000 --resolution 10
 
 # Glarus 6000x6000 (intersect on Clariden)
-dl.s2a.sh --name "europe/glarus" --offline \
-          --intersect "46.8,8.9" --tiles "32TMT,32TMS,32TNT,32TNS" \
-          --extent "475000,5170000,535000,5230000" --resolution "10" $args
+$sf --name europe/glarus --offline \
+    --intersect 46.8,8.9 --tiles 32TMT,32TMS,32TNT,32TNS \
+    --extent 475000,5170000,535000,5230000 --resolution 10
 
 # Oetztal 4000x3000 (move 5 km to the north into 32TPT)
-dl.s2a.sh --name "europe/otztal" --offline \
-          --intersect "46.8,10.9" --tiles "32TPT,32TPS" \
-          --extent "625000,5175000,665000,5205000" --resolution "10" $args
+$sf --name europe/otztal --offline \
+    --intersect 46.8,10.9 --tiles 32TPT,32TPS \
+    --extent 625000,5175000,665000,5205000 --resolution 10
 
 # Ortler 3000x3000
-dl.s2a.sh --name "europe/ortler" --offline \
-          --intersect "46.5,10.6" --tiles "32TPS" \
-          --extent "605000,5130000,635000,5160000" --resolution "10" $args
+$sf --name europe/ortler --offline \
+    --intersect 46.5,10.6 --tiles 32TPS \
+    --extent 605000,5130000,635000,5160000 --resolution 10
 
 # Adamello 3000x2000
-dl.s2a.sh --name "europe/adamello" --offline \
-          --intersect "46.2,10.6" --tiles "32TPS" \
-          --extent "605000,5105000,635000,5125000" --resolution "10" $args
+$sf --name europe/adamello --offline \
+    --intersect 46.2,10.6 --tiles 32TPS \
+    --extent 605000,5105000,635000,5125000 --resolution 10
+
 
 # Asia
 # ----
 
 # Aru Co 2000x2000
-dl.s2a.sh --name "asia/aruco" \
-          --intersect "34.0,82.3" --tiles "44SPC" \
-          --extent "605000,3755000,625000,3775000" --resolution "10" $args
+$sf --name asia/aruco \
+    --intersect 34.0,82.3 --tiles 44SPC \
+    --extent 605000,3755000,625000,3775000 --resolution 10
 
 # Haizishan 8000x10000
-dl.s2a.sh --name "asia/haizishan" \
-          --intersect "29.3,100.2" --tiles "47RNN,47RPN" --cloudcover "30" \
-          --extent "585000,3200000,665000,3300000" --resolution "10" $args
+$sf --name asia/haizishan \
+    --intersect 29.3,100.2 --tiles 47RNN,47RPN --cloudcover 30 \
+    --extent 585000,3200000,665000,3300000 --resolution 10
 
 # Hanoi 3000x3000
-dl.s2a.sh --name "asia/hanoi" \
-          --intersect "21.0,105.8" --tiles "48QWJ" --cloudcover "30" \
-          --extent "570000,2310000,600000,2340000" --resolution "10" $args
+$sf --name asia/hanoi \
+    --intersect 21.0,105.8 --tiles 48QWJ --cloudcover 30 \
+    --extent 570000,2310000,600000,2340000 --resolution 10
 
 # Vitim 4000x2000
-dl.s2a.sh --name "asia/vitim" \
-          --intersect "57.4,116.5" --tiles "50VMJ,50VNJ" --cloudcover "30" \
-          --extent "400000,6300000,600000,6400000" --resolution "50" $args
+$sf --name asia/vitim \
+    --intersect 57.4,116.5 --tiles 50VMJ,50VNJ --cloudcover 30 \
+    --extent 400000,6300000,600000,6400000 --resolution 50
+
 
 # America
 # -------
 
 # Tuya 3000x3000
-dl.s2a.sh --name "america/tuya" \
-          --intersect "59.1,-130.6" --tiles "09VUF,09VVF" --cloudcover "30" \
-          --extent "395000,6530000,425000,6560000" --resolution "10" $args
+$sf --name america/tuya \
+    --intersect 59.1,-130.6 --tiles 09VUF,09VVF --cloudcover 30 \
+    --extent 395000,6530000,425000,6560000 --resolution 10
 
--- dl.s2a.tiles.sh
+++ dl.s2a.tiles.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-# Run parent script dl.s2a.sh to prepare tile previews
-# ====================================================
+# Run sentinelflow.sh to prepare tile previews
+# ============================================
 
 ## Bash command line to list downloaded tiles
 # ls granules/ | cut -d '_' -f 10 | sort | uniq -c
 
-args="--resolution 50 --nullvalues 100 $*"
-basedir=$SCRATCH2/geodata/satellite/sentinel-2a
-prevdir=$basedir/composite/previews
-tiledir=$basedir/composite/tiles
+# move to base directory
+cd $SCRATCH2/geodata/satellite/sentinel-2a
+
+# path to executable
+sf="$HOME/code/sentinelflow/sentinelflow.sh\
+ --user julien.seguinot --pass Cordillera\
+ --resolution 50 --nullvalues 100 $*"
+
+# directories for patching
+prevdir=composite/previews
+tiledir=composite/tiles
+args="--basedir $basedir $*"
+
 
 # Alps
 # ----
@@ -32,45 +41,45 @@ tiledir=$basedir/composite/tiles
 #   for l in c:
 #       print '--intersect "%02.8f,%02.8f"' % (l[1], l[0])
 
-dl.s2a.sh $args --tiles 32TLT --name tiles/t32tlt-biel \
-                --intersect 47.38622257,07.01258040 \
-                --extent 300000,5200000,400000,5300000
-dl.s2a.sh $args --tiles 32TLS --name tiles/t32tls-lausanne \
-                --intersect 46.48686923,07.04559256 \
-                --extent 300000,5100000,400000,5200000
-dl.s2a.sh $args --tiles 32TLR --name tiles/t32tlr-aoste \
-                --intersect 45.58735656,07.07706153 \
-                --extent 300000,5000000,400000,5100000
+$sf --tiles 32TLT --name tiles/t32tlt-biel \
+    --intersect 47.38622257,07.01258040 \
+    --extent 300000,5200000,400000,5300000
+$sf --tiles 32TLS --name tiles/t32tls-lausanne \
+    --intersect 46.48686923,07.04559256 \
+    --extent 300000,5100000,400000,5200000
+$sf --tiles 32TLR --name tiles/t32tlr-aoste \
+    --intersect 45.58735656,07.07706153 \
+    --extent 300000,5000000,400000,5100000
 
-dl.s2a.sh $args --tiles 32TMT --name tiles/t32tmt-zurich \
-                --intersect 47.40153812,08.33734451 \
-                --extent 400000,5200000,500000,5300000
-dl.s2a.sh $args --tiles 32TMS --name tiles/t32tms-brig \
-                --intersect 46.50171334,08.34835925 \
-                --extent 400000,5100000,500000,5200000
-dl.s2a.sh $args --tiles 32TMR --name tiles/t32tmr-novare \
-                --intersect 45.60174443,08.35885873 \
-                --extent 400000,5000000,500000,5100000
+$sf --tiles 32TMT --name tiles/t32tmt-zurich \
+    --intersect 47.40153812,08.33734451 \
+    --extent 400000,5200000,500000,5300000
+$sf --tiles 32TMS --name tiles/t32tms-brig \
+    --intersect 46.50171334,08.34835925 \
+    --extent 400000,5100000,500000,5200000
+$sf --tiles 32TMR --name tiles/t32tmr-novare \
+    --intersect 45.60174443,08.35885873 \
+    --extent 400000,5000000,500000,5100000
 
-dl.s2a.sh $args --tiles 32TNT --name tiles/t32tnt-stgallen \
-                --intersect 47.40153812,09.66265549 \
-                --extent 500000,5200000,600000,5300000
-dl.s2a.sh $args --tiles 32TNS --name tiles/t32tns-stmoritz \
-                --intersect 46.50171334,09.65164075 \
-                --extent 500000,5100000,600000,5200000
-dl.s2a.sh $args --tiles 32TNR --name tiles/t32tnr-milano \
-                --intersect 45.60174443,09.64114127 \
-                --extent 500000,5000000,600000,5100000
+$sf --tiles 32TNT --name tiles/t32tnt-stgallen \
+    --intersect 47.40153812,09.66265549 \
+    --extent 500000,5200000,600000,5300000
+$sf --tiles 32TNS --name tiles/t32tns-stmoritz \
+    --intersect 46.50171334,09.65164075 \
+    --extent 500000,5100000,600000,5200000
+$sf --tiles 32TNR --name tiles/t32tnr-milano \
+    --intersect 45.60174443,09.64114127 \
+    --extent 500000,5000000,600000,5100000
 
-dl.s2a.sh $args --tiles 32TPT --name tiles/t32tpt-innsbruck \
-                --intersect 47.38622257,10.98741960 \
-                --extent 600000,5200000,700000,5300000
-dl.s2a.sh $args --tiles 32TPS --name tiles/t32tps-bolzano \
-                --intersect 46.48686923,10.95440744 \
-                --extent 600000,5100000,700000,5200000
-dl.s2a.sh $args --tiles 32TPR --name tiles/t32tpr-bergame \
-                --intersect 45.58735656,10.92293847 \
-                --extent 600000,5000000,700000,5100000
+$sf --tiles 32TPT --name tiles/t32tpt-innsbruck \
+    --intersect 47.38622257,10.98741960 \
+    --extent 600000,5200000,700000,5300000
+$sf --tiles 32TPS --name tiles/t32tps-bolzano \
+    --intersect 46.48686923,10.95440744 \
+    --extent 600000,5100000,700000,5200000
+$sf --tiles 32TPR --name tiles/t32tpr-bergame \
+    --intersect 45.58735656,10.92293847 \
+    --extent 600000,5000000,700000,5100000
 
 # --tiles 32TQT --intersect 47.35562341,12.31054590
 # --tiles 32TQS --intersect 46.45721117,12.25563221
@@ -119,19 +128,19 @@ find $tiledir/t32*/rgb -name "*.jpg" -printf "%f\n" | cut -d '_' -f 2-4 |
 #   for l in c:
 #       print '--intersect "%02.8f,%02.8f"' % (l[1], l[0])
 
-dl.s2a.sh $args --tiles 19XDG --name tiles/t19xdg-siorapaluk \
-                --intersect "77.91682084,-71.14010792" \
-                --extent 400000,8600000,500000,8700000
-dl.s2a.sh $args --tiles 19XDF --name tiles/t19xdf-natsilivik \
-                --intersect "77.02131528,-70.99470679" \
-                --extent 400000,8500000,500000,8600000
+$sf --tiles 19XDG --name tiles/t19xdg-siorapaluk \
+    --intersect "77.91682084,-71.14010792" \
+    --extent 400000,8600000,500000,8700000
+$sf --tiles 19XDF --name tiles/t19xdf-natsilivik \
+    --intersect "77.02131528,-70.99470679" \
+    --extent 400000,8500000,500000,8600000
 
-dl.s2a.sh $args --tiles 19XEG --name tiles/t19xeg-qeqertat \
-                --intersect "77.91682084,-66.85989208" \
-                --extent 500000,8600000,600000,8700000
-dl.s2a.sh $args --tiles 19XEF --name tiles/t19xef-qaqqarsuaq \
-                --intersect "77.02131528,-67.00529321" \
-                --extent 500000,8500000,600000,8600000
+$sf --tiles 19XEG --name tiles/t19xeg-qeqertat \
+    --intersect "77.91682084,-66.85989208" \
+    --extent 500000,8600000,600000,8700000
+$sf --tiles 19XEF --name tiles/t19xef-qaqqarsuaq \
+    --intersect "77.02131528,-67.00529321" \
+    --extent 500000,8500000,600000,8600000
 
 mkdir -p $prevdir/inglefield/{rgb,irg}
 find $tiledir/t19*/rgb -name "*.jpg" -printf "%f\n" | cut -d '_' -f 2-4 |
