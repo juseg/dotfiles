e764b9b0bf20a0e345178d42bc468cc65268ad87
Julien Seguinot <juseg@posteo.eu>
2023-02-12 09:54:51 +0100

    Print non-zero exit status in right prompt.

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,4 +1,24 @@
-function fish_right_prompt -d 'Show command duration in human readable format.'
+function fish_right_prompt -d 'Show non-zero exit status and command duration.'
+
+    # save status of last command
+    set -l last_status $status
+
+    # exit if status count was carried over (no command issued)
+    if not set -q __fish_prompt_status_generation
+        or test $__fish_prompt_status_generation = $status_generation
+            return
+    end
+
+    # save status call in global variable for next call
+    set -g __fish_prompt_status_generation $status_generation
+
+    # print non-zero exit status
+    if not contains $last_status 0 141
+        echo -n (set_color $fish_color_status)' ['
+        echo -n (set_color --bold)$last_status
+        echo -n (set_color normal)
+        echo -n (set_color $fish_color_status)'] '
+    end
 
     # split command duration in d, h, m, s
     set -l days    (math "$CMD_DURATION / 1000 / 60 / 60 / 24")
@@ -7,7 +27,7 @@ function fish_right_prompt -d 'Show command duration in human readable format.'
     set -l seconds (math "$CMD_DURATION / 1000 % 60")
 
     # set non-disturbing color
-    set_color grey
+    set_color --dim brblack
 
     # print any non-zero parts
     if test $days -gt 1
