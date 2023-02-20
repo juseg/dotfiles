b7ed9805a1a3dbbd7b7913506b6d9d5ed238f9de
Julien Seguinot <juseg@posteo.eu>
2023-02-20 19:28:59 +0100

    Fix gruvbox transparent background.

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,6 +1,9 @@
 -- ~/.config/nvim/after/plugin/colors.lua - Colorscheme ----------------------
 
-vim.g.gruvbox_italic = true
-vim.g.gruvbox_transparent_bg = true
-vim.cmd('colorscheme gruvbox')
-vim.cmd('hi Normal ctermbg=None')          -- transparent background
+-- transparent gruvbox
+require("gruvbox").setup({transparent_mode = true})
+vim.cmd.colorscheme('gruvbox')
+
+-- transparent background for any other colorscheme
+-- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none'})
+-- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none'})
