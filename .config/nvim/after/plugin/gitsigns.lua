9286b7bbdf8df4a5580b276e2c96f2558508e3af
Julien Seguinot <juseg@posteo.eu>
2023-02-21 15:23:48 +0100

    Add gitsigns buffer keymaps.

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,3 +1,33 @@
 -- ~/.config/nvim/after/plugin/gitsigns.lua - Git signs in gutter ------------
 
-require('gitsigns').setup()
+-- buffer mappings
+require('gitsigns').setup {
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        -- hunk navigation (from gitsigns readme)
+        vim.keymap.set('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+        end, { buffer = bufnr, expr = true })
+        vim.keymap.set('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+        end, { buffer = bufnr, expr = true })
+
+        -- actions on hunks
+        local opts = { buffer = bufnr }
+        vim.keymap.set({ 'n', 'v' }, '<leader>hs', gs.stage_hunk, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>hr', gs.reset_hunk, opts)
+        vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, opts)
+        vim.keymap.set('n', '<leader>hp', gs.preview_hunk, opts)
+        vim.keymap.set('n', '<leader>hb', gs.blame_line, opts)
+        vim.keymap.set('n', '<leader>hd', gs.diffthis, opts)
+
+        -- actions on buffer
+        vim.keymap.set('n', '<leader>hS', gs.stage_buffer, opts)
+        vim.keymap.set('n', '<leader>hR', gs.reset_buffer, opts)
+    end
+}
