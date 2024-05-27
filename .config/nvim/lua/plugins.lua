39ab328ccdc70939f2737671760e4afea2d2db2f
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-05-27 09:57:10 +0200

    Migrate plugins table from packer.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,42 +1,44 @@
 -- ~/.config/nvim/lua/plugins.lua - Plugin manager  --------------------------
 
-return require('packer').startup(function(use)
+-- FIXME move plugin specs along with after files to separate files
+-- (see https://github.com/folke/lazy.nvim#-structuring-your-plugins)
+return {
 
     -- plugin manager
-    use { 'wbthomason/packer.nvim' }    -- packer manages itself
+    -- 'wbthomason/packer.nvim'         -- packer manages itself
 
     -- lua plugins
-    use { 'ellisonleao/gruvbox.nvim' }  -- lua port of gruvbox colorscheme
-    use { 'folke/zen-mode.nvim' }       -- distraction-free mode
-    use { 'lewis6991/gitsigns.nvim' }   -- git signs in gutter
-    use { 'mbbill/undotree' }           -- undo tree with branches
-    use { 'nvim-lualine/lualine.nvim' } -- blazing fast status line
-    use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = {
-        { 'https://git.hubrecht.ovh/hubrecht/telescope-notmuch.nvim' },
-        { 'nvim-lua/plenary.nvim' } } } -- fuzzy file finder
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSupdate' }
-    use { 'terrortylor/nvim-comment' }  -- toggle comments
+    'ellisonleao/gruvbox.nvim',         -- lua port of gruvbox colorscheme
+    'folke/zen-mode.nvim',              -- distraction-free mode
+    'lewis6991/gitsigns.nvim',          -- git signs in gutter
+    'mbbill/undotree',                  -- undo tree with branches
+    'nvim-lualine/lualine.nvim',        -- blazing fast status line
+    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = {
+        'https://git.hubrecht.ovh/hubrecht/telescope-notmuch.nvim',
+        'nvim-lua/plenary.nvim' } },    -- fuzzy file finder
+    { 'nvim-treesitter/nvim-treesitter', build = 'TSupdate'},
+    'terrortylor/nvim-comment',         -- toggle comments
 
     -- autocompletion and lsp
     -- (FIXME choose mason auto-install or Manjaro packages)
-    use { 'VonHeikemen/lsp-zero.nvim', branch = 'v1.x', requires = {
+    { 'VonHeikemen/lsp-zero.nvim', branch = 'v1.x', dependencies = {
 
         -- LSP Support
-        {'neovim/nvim-lspconfig'},      -- language server config (required)
+        'neovim/nvim-lspconfig',        -- language server config (required)
         -- {'williamboman/mason.nvim'},           -- manage servers (optional)
         -- {'williamboman/mason-lspconfig.nvim'}, -- manage servers (optional)
 
         -- Autocompletion
-        { 'hrsh7th/cmp-nvim-lsp' },     -- complete from lsp (required)
-        { 'hrsh7th/nvim-cmp' },         -- complete engine (required)
-        { 'hrsh7th/cmp-buffer' },       -- complete from buffer (optional)
-        { 'hrsh7th/cmp-path' },         -- complete paths (optional)
-        { 'hrsh7th/cmp-nvim-lua' },     -- complete runtime nvim (optional)
-        { 'saadparwaiz1/cmp_luasnip' }, -- complete from snippets (optional)
+        'hrsh7th/cmp-nvim-lsp',         -- complete from lsp (required)
+        'hrsh7th/nvim-cmp',             -- complete engine (required)
+        'hrsh7th/cmp-buffer',           -- complete from buffer (optional)
+        'hrsh7th/cmp-path',             -- complete paths (optional)
+        'hrsh7th/cmp-nvim-lua',         -- complete runtime nvim (optional)
+        'saadparwaiz1/cmp_luasnip',     -- complete from snippets (optional)
 
         -- Snippets
-        {'L3MON4D3/LuaSnip'},             -- snippets engine (required)
-        {'rafamadriz/friendly-snippets'}, -- snippets definitions (optional)
+        'L3MON4D3/LuaSnip',             -- snippets engine (required)
+        'rafamadriz/friendly-snippets', -- snippets definitions (optional)
     } }
 
-end)
+}
