4e33ca929ab4be8733513ed07a01239ed9a24ee3
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-05-30 14:48:46 +0200

    Add keymap descriptions for which-key.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,7 +9,7 @@
 vim.g.mapleader = ' '
 
 -- remap the mode key
-vim.keymap.set('i', 'jj', '<esc>')
+vim.keymap.set('i', 'jj', '<esc>', { desc = 'exit insert mode' })
 
 -- lint, execute and make
 vim.keymap.set('n', '<F7>', '<cmd>!flake8 %<cr>') -- run flake8 on file
@@ -19,42 +19,43 @@ vim.keymap.set('n', '<F10>', '<cmd>!chmod +x %<cr>', { silent = true })
 vim.keymap.set('n', '<F12>', '<cmd>!make<cr>') -- run make
 
 -- split navigation
-vim.keymap.set('n', '<C-h>', '<C-W>h') -- move to split left
-vim.keymap.set('n', '<C-j>', '<C-W>j') -- move to split below
-vim.keymap.set('n', '<C-k>', '<C-W>k') -- move to split above
-vim.keymap.set('n', '<C-l>', '<C-W>l') -- move to split right
+vim.keymap.set('n', '<C-h>', '<C-W>h', { desc = 'move to split left' })
+vim.keymap.set('n', '<C-j>', '<C-W>j', { desc = 'move to split below' })
+vim.keymap.set('n', '<C-k>', '<C-W>k', { desc = 'move to split above' })
+vim.keymap.set('n', '<C-l>', '<C-W>l', { desc = 'move to split right' })
 
 -- split resizing
-vim.keymap.set('n', '<C-H>', '<cmd>vertical resize -4<cr>') -- Narrow window
-vim.keymap.set('n', '<C-J>', '<cmd>resize -4<cr>') -- Shorten window
-vim.keymap.set('n', '<C-K>', '<cmd>resize +4<cr>') -- Grow window
-vim.keymap.set('n', '<C-L>', '<cmd>vertical resize +4<cr>') -- Widen window
+vim.keymap.set('n', '<C-H>', '<cmd>vert res -4<cr>', { desc = 'Narrow window' })
+vim.keymap.set('n', '<C-J>', '<cmd>resize -4<cr>', { desc = 'Shorten window' })
+vim.keymap.set('n', '<C-K>', '<cmd>resize +4<cr>', { desc = 'Grow window' })
+vim.keymap.set('n', '<C-L>', '<cmd>vert res +4<cr>', { desc = 'Widen window' })
 
 -- quickfix nagivation
-vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz") -- next quickfix entry
-vim.keymap.set("n", "<C-p>", "<cmd>cprev<CR>zz") -- previous quickfix entry
+vim.keymap.set('n', '<C-n>', '<cmd>cnext<CR>zz', { desc = 'Next quickfix' })
+vim.keymap.set('n', '<C-p>', '<cmd>cprev<CR>zz', { desc = 'Previous quickfix' })
 
 -- buffer navigation
-vim.keymap.set('n', '<C-u>', '<C-u>zz') -- move up and center
-vim.keymap.set('n', '<C-d>', '<C-d>zz') -- move down and center
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up and center' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down and center' })
 
 -- copy and paste
-vim.keymap.set('n', '<leader>p', '"+p') -- paste from clipboard
-vim.keymap.set('v', '<leader>p', '"_dP') -- paste and keep
-vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d') -- delete to void register
-vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y') -- yank to system clipboard
-vim.keymap.set({ 'n', 'v' }, '<leader>x', '"_x') -- delete to void register
+vim.keymap.set('n', '<leader>p', '"+p', { desc = 'Paste from system' })
+vim.keymap.set('v', '<leader>p', '"_dP', { desc = 'Paste and keep' })
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d', { desc = 'Delete to void' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Yank to system' })
+vim.keymap.set({ 'n', 'v' }, '<leader>x', '"_x', { desc = 'Delete to void' })
 
 -- other leader key mappings
 vim.keymap.set('n', '<leader><leader>', '<cmd>so<cr>') -- source current file
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex) -- open file explorer
-vim.keymap.set("n", "<leader>s", -- search and replace word under curser
-    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set('n', '<leader>s',
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = 'Replace current word' })
 
 -- reduce cursor motion
-vim.keymap.set('n', 'J', 'mzJ`z') -- join lines without moving cursor
-vim.keymap.set('n', 'n', 'nzzzv') -- search forward, center, and unfold
-vim.keymap.set('n', 'N', 'Nzzzv') -- search backward, center, and unfold
-vim.keymap.set('n', 'Q', '<nop>') -- disable repeat last register
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- move selection down
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv") -- move selection up
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines without moving' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Search forward and center' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Search backward and center' })
+vim.keymap.set('n', 'Q', '<nop>', { desc = 'Disable repeat last register' })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
