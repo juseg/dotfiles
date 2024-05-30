252328b6485dba1ee4844332ad1ef97cb32eef4c
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-05-30 15:38:39 +0200

    Move lazy bootstrap to lua/config/plugins.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,19 +3,8 @@
 --
 -- ~/.config/nvim/init.lua - Neovim configuration file using lua script
 
--- load neovim base configuration
+-- load neovim configuration
 require('config.autocmd') -- automatic commands
 require('config.keymaps') -- key mappings
 require('config.options') -- neovim settings
-
--- bootstrap lazy (see https://github.com/folke/lazy.nvim#-installation)
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    'git', 'clone', '--branch=stable', '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git', lazypath})
-end
-vim.opt.rtp:prepend(lazypath)
-
--- load specs (https://github.com/folke/lazy.nvim#-structuring-your-plugins)
-require('lazy').setup('plugin', { checker = { enabled = true, notify = false }})
+require('config.plugins') -- plugin manager
