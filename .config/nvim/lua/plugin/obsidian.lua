0934728b0ff8477dfdd8c1752271cd534a030e28
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-06-21 15:06:34 +0200

    Add basic configuration.

--- .config/nvim/lua/plugin/obsidian.lua
+++ .config/nvim/lua/plugin/obsidian.lua
@@ -9,8 +9,25 @@ return {
   init = function() vim.opt.conceallevel = 1 end, -- conceal checkboxes etc
   version = '*', -- recommended, use latest release instead of latest commit
   ft = 'markdown', -- load obsidian on any markdown file
-  dependencies = { 'nvim-lua/plenary.nvim' }, -- required.
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'hrsh7th/nvim-cmp',      -- optional, complete links and tags
+  },
+  keys = {
+    { '<leader>od', '<cmd>ObsidianDailies<cr>', desc = 'Open daily note' },
+    { '<leader>on', '<cmd>ObsidianNew<cr>', desc = 'Create new note' },
+    { '<leader>oo', '<cmd>ObsidianOpen<cr>', desc = 'Open in Obsidian' },
+      -- check README for more commands
+  },
   opts = {
     disable_frontmatter = true, -- don't manage frontmatter for now
-    workspaces = { { name = 'vault', path = '~/git/note/vault' } } },
+    workspaces = { { name = 'vault', path = '~/git/note/vault' } },
+
+    daily_notes = {
+        alias_format = '%a %-d %b %Y',  -- title of new daily note
+        date_format = '%Y/%y%m%d',      -- path to daily notes
+        folder = 'diary/daily',         -- daily notes folder
+        -- template = 'daily.md',       -- template for new notes
+    },
+  },
 }
